66e9d556dac4f2c6ade627f0d2883719d8bd0e39
jfpa11100 <felipepala1000@gmail.com>
2023-05-23 10:55:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using AdapterChallenge;
+
+//Client 
+string[,] employeesArray = new string[5, 4]
+            {
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
+            };
+
+//Create the EmployeeAdapter who Makes it possible to work with Two Incompatible Interfaces
+
+
